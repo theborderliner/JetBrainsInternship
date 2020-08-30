32baa6b1b01c29e7c5a7980fe16cbe7c36ec59dc
the_borderliner <ivan.peshekhonov@gmail.com>
2020-08-30 18:59:09 +0300

    Fix button block

--- src/blocks/content/button/button.js
+++ src/blocks/content/button/button.js
@@ -1,12 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     var buttons = document.getElementsByClassName("button");
-    console.log(buttons);
 
     for (let i = 0; i < buttons.length; i++) {
         let button = buttons[i];
-        console.log(button);
         button.addEventListener("mousedown", (e) => {
-
             e.currentTarget.classList.add("button_pressed");
         });
         button.addEventListener("mouseup", (e) => {
