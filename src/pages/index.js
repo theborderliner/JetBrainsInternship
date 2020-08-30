111c30c454f3f5b90ce48f9367f8ce01801cec2d
the_borderliner <ivan.peshekhonov@gmail.com>
2020-08-30 17:51:50 +0300

    Add first screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,16 @@
 require("./common.scss");
+
+require("../blocks/theme/theme.scss")
+
+require("../blocks/skeleton/layout/layout.scss")
+require("../blocks/skeleton/grid/grid.scss")
+require("../blocks/skeleton/screen/screen.scss")
+
+require("../blocks/content/text/text.scss")
+require("../blocks/content/image/image.scss")
+require("../blocks/content/button/button.js")
+require("../blocks/content/button/button.scss")
+require("../blocks/content/switcher/switcher.scss")
+
+require("../blocks/template/header/header.scss")
+require("../blocks/template/first-scren/first-screen.scss")
