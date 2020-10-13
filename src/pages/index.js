9ca3a2e6b2eebd8299b49f1a2a4ab1a129fe34a9
JohanDDC <ivan.peshekhonov@gmail.com>
2020-10-13 08:48:08 +0300

    Badum tssss

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ require("../blocks/content/image/image.scss")
 require("../blocks/content/section/section.scss")
 require("../blocks/content/arrow-link/arrow-link.scss")
 require("../blocks/content/slider/slider.scss")
+require("../blocks/content/text-input/text-input.scss")
 
 require("../blocks/content/button/button") // Button js
 require("../blocks/content/button/button.scss") // Button styles
@@ -28,10 +29,16 @@ require("../blocks/template/first-scren/first-screen.scss")
 require("../blocks/template/icon/icon.scss")
 require("../blocks/template/booklet-slider/booklet-slider.scss")
 
+require("../blocks/template/contact-info/contact-info.scss")
+require("../blocks/template/contact-info/contact-info")
+
 require("../blocks/template/theme-switcher/theme-switcher") // Theme switcher js
 require("../blocks/template/theme-switcher/theme-switcher.scss") // Theme switcher styles
 
 require("../blocks/template/footer/footer.scss") // Footer
+
+require("../blocks/template/get-in-touch-button/get-in-touch-button")
+
 
 // Description screen
 require("../blocks/template/description-screen/description-screen.scss")
